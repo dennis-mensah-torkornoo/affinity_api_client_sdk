316366c176539e0eba6186dfdefe8d0c56519bc0
Dennis M T <dennismends@gmail.com>
2021-12-06 06:42:41 +0000

    Regenerate api method stubs

--- lib/affinity_api_client_sdk.dart
+++ lib/affinity_api_client_sdk.dart
@@ -8,14 +8,20 @@ import 'package:affinity_api_client_sdk/agency_api/lib/api.dart'
         DefaultApi,
         TasksApi,
         ApiClient;
+import 'package:affinity_api_client_sdk/utility_api/lib/api.dart' as utility_api ;
 import 'package:http_interceptor/http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 
 class AgencyApiClient {
   late ApiClient apiClient;
+  late utility_api.ApiClient utilityApiClient;
+
   AgencyApiClient(
       [List<InterceptorContract>? interceptors, RetryPolicy? retryPolicy]) {
     apiClient = ApiClient(basePath: 'https://api.affinitylabstest.com/agency')
+      ..client = InterceptedClient.build(
+          interceptors: interceptors ?? [], retryPolicy: retryPolicy);
+    utilityApiClient = utility_api.ApiClient(basePath: 'https://api.affinitylabstest.com/utility')
       ..client = InterceptedClient.build(
           interceptors: interceptors ?? [], retryPolicy: retryPolicy);
   }
@@ -25,4 +31,5 @@ class AgencyApiClient {
   AuthenticationApi get authenticationApi => AuthenticationApi(apiClient);
   DefaultApi get defaultApi => DefaultApi(apiClient);
   TasksApi get tasksApi => TasksApi(apiClient);
+  utility_api.DefaultApi get utilityApi => utility_api.DefaultApi(utilityApiClient);
 }
